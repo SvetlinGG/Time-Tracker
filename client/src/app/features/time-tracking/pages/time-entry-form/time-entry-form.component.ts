bbdbf0dcc539c0ff6866adf371edb89cc2d65684
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-02 11:11:45 +0200

    create time-entry-form and time entry-list components

---
+++ client/src/app/features/time-tracking/pages/time-entry-form/time-entry-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-entry-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-entry-form.component.html',
+  styleUrl: './time-entry-form.component.css'
+})
+export class TimeEntryFormComponent {
+
+}
---
+++ client/src/app/features/time-tracking/pages/time-entry-list/time-entry-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-entry-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-entry-list.component.html',
+  styleUrl: './time-entry-list.component.css'
+})
+export class TimeEntryListComponent {
+
+}
