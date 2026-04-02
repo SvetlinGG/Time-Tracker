81a091052e576e83520059545a6608fb05a959aa
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-02 11:16:37 +0200

    create payroll-list, reports-home and settings-home components

---
+++ client/src/app/features/reports/pages/reports-home/reports-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './reports-home.component.html',
+  styleUrl: './reports-home.component.css'
+})
+export class ReportsHomeComponent {
+
+}
