2959a364367e03f49ae35c0800fce69b2037adb5
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-02 11:53:27 +0200

    make routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', 
+        loadComponent: () => 
+            import('./features/auth/pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {path: '',
+        loadComponent: () =>
+            import('./core/layouts/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => 
+                    import('./features/dashboard/pages/dashboard-home/dashboard-home.component').then(
+                        m => m.DashboardHomeComponent)
+            }
+        ]
+    }
+];
