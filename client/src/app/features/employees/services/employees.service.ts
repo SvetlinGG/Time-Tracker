6833ab76bbed134722a81e70b4805dec33f7fa4a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-07 15:10:24 +0200

    CRUD operation on employee service

--- client/src/app/features/employees/services/employees.service.ts
+++ client/src/app/features/employees/services/employees.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Employee } from '../../../core/models/employee.model';
+import { CreateEmployeeDto, Employee } from '../../../core/models/employee.model';
 
 @Injectable({providedIn: 'root'})
 export class EmployeesService {
@@ -9,11 +9,23 @@ export class EmployeesService {
   private http = inject(HttpClient);
   private readonly baseUrl = 'http://localhost:3000/apiemployees';
 
-  getAll(): Observable<Employee>{
+  getAll(): Observable<Employee[]>{
     return this.http.get<Employee[]>(this.baseUrl)
   }
 
   getById(id: string): Observable<Employee>{
     return this.http.get<Employee>(`${this.baseUrl}/${id}`)
+  }
+
+  create(payload: CreateEmployeeDto): Observable<Employee>{
+    return this.http.post<Employee>(this.baseUrl, payload)
+  }
+
+  update(id: string, payload: CreateEmployeeDto): Observable<Employee>{
+    return this.http.put<Employee>(`${this.baseUrl}/${id}`, payload)
+  }
+
+  delete(id: string): Observable<void>{
+    return this.http.delete<void>(`${this.baseUrl}/${id}`)
   }
 }
