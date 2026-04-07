0d49590d81a962cbdf299e4fadff92263c980c66
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-07 15:04:14 +0200

    get method on employee service

--- client/src/app/features/employees/services/employees.service.ts
+++ client/src/app/features/employees/services/employees.service.ts
@@ -1,9 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../../../core/models/employee.model';
 
 @Injectable({providedIn: 'root'})
 export class EmployeesService {
 
   private http = inject(HttpClient);
-  private readonly baseUrl = 'http://localhost:3000/apiemployees'
+  private readonly baseUrl = 'http://localhost:3000/apiemployees';
+
+  getAll(): Observable<Employee>{
+    return this.http.get<Employee[]>(this.baseUrl)
+  }
+
+  getById(id: string): Observable<Employee>{
+    return this.http.get<Employee>(`${this.baseUrl}/${id}`)
+  }
 }
