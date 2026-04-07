a1e5aa9a981ada22ce75a08daa447371a976c18b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-07 23:28:25 +0200

    setup imports

--- client/src/app/features/employees/pages/employees-list/employees-list.component.ts
+++ client/src/app/features/employees/pages/employees-list/employees-list.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employees-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './employees-list.component.html',
   styleUrl: './employees-list.component.css'
 })
