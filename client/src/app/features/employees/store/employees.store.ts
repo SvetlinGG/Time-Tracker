af200d72cf1954c9ac96decd4efabb8fa6c8a244
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-07 22:56:52 +0200

    more of employees store

--- client/src/app/features/employees/store/employees.store.ts
+++ client/src/app/features/employees/store/employees.store.ts
@@ -1,6 +1,7 @@
-import { inject, Injectable, signal } from "@angular/core";
+import { computed, inject, Injectable, signal } from "@angular/core";
 import { EmployeesService } from "../services/employees.service";
 import { Employee } from "../../../core/models/employee.model";
+import { finalize } from "rxjs";
 
 
 
@@ -20,4 +21,27 @@ export class EmployeesStore {
     readonly loading = this._loading.asReadonly();
     readonly submitting = this._submitting.asReadonly();
     readonly error = this._error.asReadonly();
+
+    readonly activeEmployees = computed(() => 
+    this._employees().filter(employee => employee.isActive)
+    );
+
+    readonly employeesCount = computed(() => this._employees().length);
+
+    loadEmployees():void {
+        this._loading.set(true);
+        this._error.set(null);
+
+        this.employeesService
+            .getAll()
+            .pipe(finalize(() => this._loading.set(false)))
+            .subscribe({
+                next: employees => this._employees.set(employees),
+                error: () => this._error.set('Failed to load employees.'),
+            });
+    }
+
+    loadEmployeeById(id: string, callback: (employee: Employee) => void): void{
+        
+    }
 }
