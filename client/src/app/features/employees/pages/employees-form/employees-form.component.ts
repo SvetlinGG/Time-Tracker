ea4c6e2cb9a221b0ff93d45b8de4329fdfb59b60
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-08 21:16:08 +0200

    add and edit data

--- client/src/app/features/employees/pages/employees-form/employees-form.component.ts
+++ client/src/app/features/employees/pages/employees-form/employees-form.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { EmployeesStore } from '../../store/employees.store';
+import { ContractType } from '../../../../core/models/employee.model';
 
 @Component({
   selector: 'app-employees-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './employees-form.component.html',
   styleUrl: './employees-form.component.css'
 })
-export class EmployeesFormComponent {
+export class EmployeesFormComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private store = inject(EmployeesStore);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
+  private employeeId = signal<string | null>(null);
+
+
+  readonly contractTypes: ContractType[] = ['hourly', 'monthly', 'shift'];
+  readonly submitting = this.store.submitting;
+  readonly error = this.store.error;
+
+  readonly form = this.fb.nonNullable.group({
+    firstName: ['', [Validators.required, Validators.minLength(2)]],
+    lastName: ['', Validators.required, Validators.minLength(2)],
+    personalNumber: ['', Validators.required, Validators.minLength(3)],
+    
+  })
+  
 }
