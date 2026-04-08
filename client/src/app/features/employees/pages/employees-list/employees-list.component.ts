a2d0f51ad74b8f446821c56803611528b2b2f8a2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-08 14:22:09 +0200

    employees-list data

--- client/src/app/features/employees/pages/employees-list/employees-list.component.ts
+++ client/src/app/features/employees/pages/employees-list/employees-list.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { EmployeesStore } from '../../store/employees.store';
+import { Employee } from '../../../../core/models/employee.model';
+
 
 @Component({
   selector: 'app-employees-list',
@@ -9,6 +12,47 @@ import { RouterLink } from '@angular/router';
   templateUrl: './employees-list.component.html',
   styleUrl: './employees-list.component.css'
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
+
+  private store = inject(EmployeesStore);
+  private router = inject(Router);
+
+  readonly searchTerm = signal('');
+  readonly employees = this.store.employees;
+  readonly loading = this.store.loading;
+  readonly error = this.store.error;
+
+  readonly filteredEmployees = computed(() => {
+    const term = this.searchTerm().trim().toLowerCase();
+
+    if(!term){
+      return this.employees();
+    }
+  })
+
+    
+  
+  ngOnInit(): void {
+    this.store.loadEmployees();
+  }
+
+  onSearch(value: string): void{
+    this.searchTerm.set(value);
+  }
+
+  onEdit(employee: Employee): void{
+    this.router.navigate((['./employees', employee._id, 'edit']));
+  }
+
+  onDelete(employee: Employee): void {
+    const confirmed = confirm(
+      `Are you sure you want to delete ${employee.firstName} ${employee.lastName}?`
+    );
+    if (!confirmed){
+      return;
+    }
+  }
+
+
 
 }
