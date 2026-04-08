1b2a0b8cd0256becd044072d0de723440addcdfa
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-08 21:50:38 +0200

    employee html build

--- client/src/app/features/employees/pages/employees-form/employees-form.component.ts
+++ client/src/app/features/employees/pages/employees-form/employees-form.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, computed } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { EmployeesStore } from '../../store/employees.store';
